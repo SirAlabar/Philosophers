419669c76731803f2e9753753f07f8aa27328bae
Hugo Marta <150078628+SirAlabar@users.noreply.github.com>
2024-11-27 17:55:57 +0000

    fexid dead with low delay

--- src/monitor.c
+++ src/monitor.c
@@ -31,35 +31,27 @@ bool	all_ate_enough(t_data *data)
 
 void	*monitor_routine(void *data_void)
 {
-    t_data *data = (t_data *)data_void;
-    int i;
-    int all_eaten;
+   t_data *data = (t_data *)data_void;
+   int i;
+   long long time;
 
-    while (1)
-    {
-        i = -1;
-        all_eaten = 0;
-        while (++i < data->num_philos)
-        {
-            if (time_diff(data->philosophers[i].last_meal, get_time()) >= data->time_to_die)
-            {
-                pthread_mutex_lock(&data->death_mutex);
-                data->someone_died = true;
-                print_status(&data->philosophers[i], DIED);
-                pthread_mutex_unlock(&data->death_mutex);
-                return (NULL);
-            }
-            if (data->must_eat != -1 && data->philosophers[i].meals_eaten >= data->must_eat)
-                all_eaten++;
-        }
-        if (data->must_eat != -1 && all_eaten >= data->num_philos)
-        {
-            pthread_mutex_lock(&data->death_mutex);
-            data->someone_died = true;
-            pthread_mutex_unlock(&data->death_mutex);
-            return (NULL);
-        }
-        usleep(1000);
-    }
-    return (NULL);
+   while (1)
+   {
+       i = -1;
+       while (++i < data->num_philos)
+       {
+           time = time_diff(data->philosophers[i].last_meal, get_time());
+           if (time >= data->time_to_die && !check_death(data))
+           {
+               pthread_mutex_lock(&data->death_mutex);
+               data->someone_died = true;
+               pthread_mutex_unlock(&data->death_mutex);
+               printf("%lld %d died\n", time_diff(data->start_time, get_time()), data->philosophers[i].id);
+               return (NULL);
+           }
+       }
+       usleep(50);
+       if (check_death(data))
+           return (NULL);
+   }
 }
